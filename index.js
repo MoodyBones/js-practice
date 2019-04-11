8e343bea426f118406d5f3f0bef24fc5a574a84e
Mel <jones.mel@gmail.com>
2019-04-11 19:42:30 +0200

    10-Apr Excercises

--- index.js
+++ index.js
@@ -176,3 +176,18 @@
 // console.log(array2);
 // var getOrange = array2[1][1][0];
 // console.log(getOrange);
+
+
+//// MEL CONTINUE FROM HERE!!!
+
+
+// EXCERCISE 7
+
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
