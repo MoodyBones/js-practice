14c2aeb72ce90fa0872ac83f82fe0e7fb256ccc9
Mel <jones.mel@gmail.com>
2019-04-11 19:34:26 +0200

    11-Apr Excercises

--- car.js
+++ car.js
@@ -14,14 +14,14 @@
 // "Powering On. Enjoy the ride!"
 
 
-var age = prompt("How old are you?");
-if (Number(age) < 18) {
-    alert("Sorry, you are too young to drive this car. Powering off");
-} else if (Number(age) === 18) {
-    alert("Congratulations on your first year of driving. Enjoy the ride!");
-} else if (Number(age) > 18) {
-    alert("Powering On. Enjoy the ride!");
-}
+// var age = prompt("How old are you?");
+// if (Number(age) < 18) {
+//     alert("Sorry, you are too young to drive this car. Powering off");
+// } else if (Number(age) === 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+// } else if (Number(age) > 18) {
+//     alert("Powering On. Enjoy the ride!");
+// }
 
 
 // testing little feature
