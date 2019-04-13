269cc288ebff18fb42cd40ae564bfa8e8fd5ca12
Mel <jones.mel@gmail.com>
2019-04-13 20:16:53 +0200

    Shopping List excercise complete

--- DOM events/script.js
+++ DOM events/script.js
@@ -1,24 +1,38 @@
-var button = document.getElementById("enter");
+var enterButt = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-let lis = document.getElementsByTagName("li");
+var listButton = document.querySelectorAll('.delete--button');
+
 
 function inputLength() {
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+function createListElement(el) {
+	var li, inputText, btn;
+	li = document.createElement("li");
+	inputText = document.createTextNode(input.value);
+	btn = document.createElement("button");
+
+	btn.appendChild(document.createTextNode("Delete"));
+	li.appendChild(inputText);
+	li.appendChild(btn);
 	ul.appendChild(li);
+	
 	input.value = "";
+	btn.onclick = deleteListItem;
+
 }
+
+
+
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
+
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
@@ -32,8 +46,19 @@ function doneItem(e) {
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+function deleteListItem(event) {
+	var listElement = event.target.parentNode;
+	event.target.parentNode.parentNode.removeChild(listElement);
+}
+
+
+enterButt.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener("click", doneItem);
+
+Array.from(listButton).forEach(link => {
+    link.addEventListener('click', deleteListItem);
+});
+
